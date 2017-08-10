dfd60cd32a0f34622a7291648e361e0d4a5020ef
shrividluck <shrividluck@gmail.com>
2017-08-09 22:26:28 -0700

    Updated with Edit ToDoItem Activity :-) !

--- app/src/main/java/com/shrividya/simpletodo/MainActivity.java
+++ app/src/main/java/com/shrividya/simpletodo/MainActivity.java
@@ -1,5 +1,7 @@
 package com.shrividya.simpletodo;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -7,6 +9,7 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import org.apache.commons.io.FileUtils;
 
@@ -19,45 +22,32 @@ public class MainActivity extends AppCompatActivity {
     ArrayList<String> items;
     ArrayAdapter<String> itemsAdapter;
     ListView lvItems;
+    private final int REQUEST_CODE = 200;
+    EditText etNewItem;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         lvItems = (ListView) findViewById(R.id.lvItems);
-        //items = new ArrayList<>();
         readItems();
         itemsAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, items);
         lvItems.setAdapter(itemsAdapter);
-       /* items.add("First item");
-        items.add("Second item");*/
         setUpListViewListener();
+
     }
 
     public void onAddItem(View v) {
-        EditText etNewItem = (EditText) findViewById(R.id.etNewItem);
+        etNewItem = (EditText) findViewById(R.id.etNewItem);
         String itemText = etNewItem.getText().toString();
         itemsAdapter.add(itemText);
         etNewItem.setText("");
         writeItems();
     }
 
-    private void setUpListViewListener() {
-        lvItems.setOnItemLongClickListener(
-                    new AdapterView.OnItemLongClickListener() {
-                @Override
-                public boolean onItemLongClick(AdapterView<?> adapter, View item, int pos, long id) {
-                    items.remove(pos);
-                    itemsAdapter.notifyDataSetChanged();
-                    writeItems();
-                    return true;
-                }
 
-        });
-    }
-
-
-    private void writeItems() {
+    public void writeItems() {
         File filesDir = getFilesDir();
         File todoFile = new File(filesDir, "todo.txt");
         try {
@@ -69,7 +59,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    private void readItems() {
+    public void readItems() {
         File filesDir = getFilesDir();
         File todoFile = new File(filesDir, "todo.txt");
         try {
@@ -78,4 +68,44 @@ public class MainActivity extends AppCompatActivity {
             items = new ArrayList<String>();
         }
     }
+
+    public void setUpListViewListener() {
+        lvItems.setOnItemLongClickListener(
+                    new AdapterView.OnItemLongClickListener() {
+                    @Override
+                    public boolean onItemLongClick(AdapterView<?> adapter, View item, int pos, long id) {
+                        items.remove(pos);
+                        itemsAdapter.notifyDataSetChanged();
+                        writeItems();
+                        return true;
+                }
+
+        });
+        lvItems.setOnItemClickListener(
+                    new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> adapter, View item, int pos, long id) {
+                        Intent i = new Intent(MainActivity.this, EditItemActivity.class);
+                        i.putExtra("mode", pos);
+                        i.putExtra("items", items);
+                        startActivityForResult(i, REQUEST_CODE);
+                    }
+                    });
+    }
+
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK && requestCode == REQUEST_CODE) {
+            String newItem = data.getExtras().getString("EditedText");
+            int code = data.getExtras().getInt("code", 0);
+            items = data.getExtras().getStringArrayList("EditedArrList");
+            writeItems();
+            itemsAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, items);
+            lvItems.setAdapter(itemsAdapter);
+            Toast.makeText(this,newItem,Toast.LENGTH_SHORT).show();
+
+        }
+    }
+
 }
